209fde2a16ae967d11b090fb4189c48820b9162a
gendorx <114492601+gendorx@users.noreply.github.com>
2023-05-27 22:27:02 +0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ import Api from "./api";
 const apiConfig = {
   baseUrl: "https://mesto.nomoreparties.co/v1/cohort-60",
   headers: {
-    authorization: "3c12d929-d321-40f7-9ea5-47a9cc856981",
+    authorization: "3c12d929-d321-40f7-9ea5-47a9cc856911",
     "Content-Type": "application/json",
   },
 };
